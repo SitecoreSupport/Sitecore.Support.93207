cff1023ab4cbe7cf828f0eba2ef264f25f93e186
Dmitry Kolinchuk <dmko@sitecore.net>
2017-09-21 12:46:07 +0300

    Import original code of Sitecore XP 8.2 rev. 170728 (Update-5)

---
+++ src/Sitecore.Support.93207/AddFromTemplate.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Diagnostics;
+using System;
+
+namespace Sitecore.Shell.Framework.Commands
+{
+    /// <summary>
+    /// Represents the AddFromTemplate command.
+    /// </summary>
+    [Serializable]
+    public class AddFromTemplate : Command
+    {
+        /// <summary>
+        /// Executes the command in the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <contract>
+        ///   <requires name="context" condition="not null" />
+        /// </contract>
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            if (context.Items.Length != 1 || !context.Items[0].Access.CanCreate())
+            {
+                return;
+            }
+            Items.AddFromTemplate(context.Items[0]);
+        }
+
+        /// <summary>
+        /// Queries the state of the command.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>The state of the command.</returns>
+        /// <contract>
+        ///   <requires name="context" condition="not null" />
+        /// </contract>
+        public override CommandState QueryState(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            if (context.Items.Length != 1)
+            {
+                return CommandState.Hidden;
+            }
+            if (!context.Items[0].Access.CanCreate() || !context.Items[0].Access.CanWriteLanguage())
+            {
+                return CommandState.Disabled;
+            }
+            return base.QueryState(context);
+        }
+    }
+}
---
+++ src/Sitecore.Support.93207/AddMaster.cs
@@ -0,0 +1,157 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data.Events;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Exceptions;
+using Sitecore.Globalization;
+using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+
+namespace Sitecore.Shell.Framework.Commands
+{
+    /// <summary>
+    /// Represents the AddMaster command.
+    /// </summary>
+    [Serializable]
+    public class AddMaster : Command
+    {
+        /// <summary>
+        /// Occurs when item created.
+        /// </summary>
+        [method: CompilerGenerated]
+        [CompilerGenerated]
+        protected event ItemCreatedDelegate ItemCreated;
+
+        /// <summary>
+        /// Executes the command in the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public override void Execute(CommandContext context)
+        {
+            if (context.Items.Length == 1 && context.Items[0].Access.CanCreate())
+            {
+                Item item = context.Items[0];
+                NameValueCollection nameValueCollection = new NameValueCollection();
+                nameValueCollection["Master"] = context.Parameters["master"];
+                nameValueCollection["ItemID"] = item.ID.ToString();
+                nameValueCollection["Language"] = item.Language.ToString();
+                nameValueCollection["Version"] = item.Version.ToString();
+                Context.ClientPage.Start(this, "Add", nameValueCollection);
+            }
+        }
+
+        /// <summary>
+        /// Queries the state of the command.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>The state of the command.</returns>
+        public override CommandState QueryState(CommandContext context)
+        {
+            Error.AssertObject(context, "context");
+            if (context.Items.Length != 1)
+            {
+                return CommandState.Hidden;
+            }
+            if (!context.Items[0].Access.CanCreate())
+            {
+                return CommandState.Disabled;
+            }
+            return base.QueryState(context);
+        }
+
+        /// <summary>
+        /// Adds the specified args.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        protected void Add(ClientPipelineArgs args)
+        {
+            if (!SheerResponse.CheckModified())
+            {
+                return;
+            }
+            Item item = Context.ContentDatabase.GetItem(args.Parameters["Master"]);
+            if (item == null)
+            {
+                SheerResponse.Alert(Translate.Text("Branch \"{0}\" not found.", new object[]
+                {
+                    args.Parameters["Master"]
+                }), new string[0]);
+                return;
+            }
+            if (!(item.TemplateID == TemplateIDs.CommandMaster))
+            {
+                if (args.IsPostBack)
+                {
+                    if (args.HasResult)
+                    {
+                        string @string = StringUtil.GetString(new string[]
+                        {
+                            args.Parameters["ItemID"]
+                        });
+                        string string2 = StringUtil.GetString(new string[]
+                        {
+                            args.Parameters["Language"]
+                        });
+                        Item item2 = Context.ContentDatabase.Items[@string, Language.Parse(string2)];
+                        if (item2 == null)
+                        {
+                            SheerResponse.Alert("Parent item not found.", new string[0]);
+                            return;
+                        }
+                        if (!item2.Access.CanCreate())
+                        {
+                            Context.ClientPage.ClientResponse.Alert("You do not have permission to create items here.");
+                            return;
+                        }
+                        Item item3 = null;
+                        try
+                        {
+                            if (item.TemplateID == TemplateIDs.BranchTemplate)
+                            {
+                                BranchItem branchItem = item;
+                                item3 = Context.Workflow.AddItem(args.Result, branchItem, item2);
+                                Log.Audit(this, "Add from branch: {0}", new string[]
+                                {
+                                    AuditFormatter.FormatItem(branchItem)
+                                });
+                            }
+                            else
+                            {
+                                TemplateItem templateItem = item;
+                                item3 = Context.Workflow.AddItem(args.Result, templateItem, item2);
+                                Log.Audit(this, "Add from template: {0}", new string[]
+                                {
+                                    AuditFormatter.FormatItem(templateItem)
+                                });
+                            }
+                        }
+                        catch (WorkflowException ex)
+                        {
+                            Log.Error("Workflow error: could not add item from master", ex, this);
+                            SheerResponse.Alert(ex.Message, new string[0]);
+                        }
+                        if (item3 != null && this.ItemCreated != null)
+                        {
+                            this.ItemCreated(this, new ItemCreatedEventArgs(item3));
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    SheerResponse.Input("Enter a name for the new item:", item.DisplayName, Settings.ItemNameValidation, "'$Input' is not a valid name.", Settings.MaxItemNameLength);
+                    args.WaitForPostBack();
+                }
+                return;
+            }
+            string text = item["Command"];
+            if (string.IsNullOrEmpty(text))
+            {
+                return;
+            }
+            Context.ClientPage.SendMessage(this, text);
+        }
+    }
+}
