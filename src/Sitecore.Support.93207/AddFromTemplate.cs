8e253b039aba4f0fd92aad5c34bf905db2a739fe
DK\tpe <tpe@sitecore.net>
2018-08-28 13:20:39 +0300

    Importing original code from 9.0 Update-1

---
+++ src/Sitecore.Support.93207/AddFromTemplate.cs
@@ -0,0 +1,119 @@
+﻿namespace Sitecore.Support.Shell.Framework.Pipelines
+{
+
+  using Sitecore.Configuration;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Globalization;
+  using Sitecore.Shell.Framework;
+  using Sitecore.Text;
+  using Sitecore.Web.UI.Sheer;
+  using System;
+
+  public class AddFromTemplate
+  {
+    // Methods
+    protected virtual Item AddItemFromBranch(string name, BranchItem branch, Item parent) =>
+        Context.Workflow.AddItem(name, branch, parent);
+
+    protected virtual Item AddItemFromTemplate(string name, TemplateItem template, Item parent) =>
+        Context.Workflow.AddItem(name, template, parent);
+
+    protected virtual void AuditMessage(object owner, string format, params string[] parameters)
+    {
+      Log.Audit(owner, format, parameters);
+    }
+
+    public void Execute(ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (args.HasResult)
+      {
+        int index = args.Result.IndexOf(',');
+        Assert.IsTrue(index >= 0, "Invalid return value from dialog");
+        string path = StringUtil.Left(args.Result, index);
+        string name = StringUtil.Mid(args.Result, index + 1);
+        Database database = this.GetDatabase(args.Parameters["database"]);
+        string str3 = args.Parameters["id"];
+        string str4 = args.Parameters["language"];
+        Item parent = database.GetItem(str3, Language.Parse(str4));
+        if (parent == null)
+        {
+          SheerResponse.Alert("Parent item not found.", Array.Empty<string>());
+          args.AbortPipeline();
+        }
+        else if (!parent.Access.CanCreate())
+        {
+          SheerResponse.Alert("You do not have permission to create items here.", Array.Empty<string>());
+          args.AbortPipeline();
+        }
+        else
+        {
+          Item item = database.GetItem(path, Language.Parse(str4));
+          if (item == null)
+          {
+            SheerResponse.Alert("Item not found.", Array.Empty<string>());
+            args.AbortPipeline();
+          }
+          else
+          {
+            Item item3;
+            this.SetHistoryTemplate(item.ID);
+            if (item.TemplateID == TemplateIDs.Template)
+            {
+              string[] parameters = new string[] { AuditFormatter.FormatItem(item) };
+              this.AuditMessage(this, "Add from template: {0}", parameters);
+              TemplateItem template = item;
+              item3 = this.AddItemFromTemplate(name, template, parent);
+            }
+            else
+            {
+              string[] parameters = new string[] { AuditFormatter.FormatItem(item) };
+              this.AuditMessage(this, "Add from branch: {0}", parameters);
+              BranchItem branch = item;
+              item3 = this.AddItemFromBranch(name, branch, parent);
+            }
+            args.CarryResultToNextProcessor = true;
+            if (item3 == null)
+            {
+              args.AbortPipeline();
+            }
+            else
+            {
+              args.Result = item3.ID.ToString();
+            }
+          }
+        }
+      }
+    }
+
+    protected virtual Database GetDatabase(string databaseName)
+    {
+      Assert.ArgumentNotNullOrEmpty(databaseName, "databaseName");
+      return Factory.GetDatabase(databaseName);
+    }
+
+    public void GetTemplate(ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (SheerResponse.CheckModified())
+      {
+        UrlString str = new UrlString(UIUtil.GetUri("control:AddFromTemplate"));
+        string template = History.Template;
+        if ((template != null) && (template.Length > 0))
+        {
+          str.Append("fo", template);
+        }
+        Context.ClientPage.ClientResponse.ShowModalDialog(str.ToString(), "1200px", "700px", "", true);
+        args.WaitForPostBack(false);
+      }
+    }
+
+    protected virtual void SetHistoryTemplate(ID templateId)
+    {
+      History.Template = templateId.ToString();
+    }
+  }
+
+}
---
+++ src/Sitecore.Support.93207/AddMaster.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Sitecore.Support.Shell.Framework.Commands
+{
+  using Sitecore.Data.Events;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Exceptions;
+  using Sitecore.Globalization;
+  using Sitecore.Shell.Framework.Commands;
+  using Sitecore.Web.UI.Sheer;
+  using System;
+  using System.Collections.Specialized;
+  using System.Runtime.CompilerServices;
+
+  [Serializable]
+  public class AddMaster : Command
+  {
+    // Events
+    [field: CompilerGenerated]
+    protected event ItemCreatedDelegate ItemCreated;
+
+    // Methods
+    protected void Add(ClientPipelineArgs args)
+    {
+      if (SheerResponse.CheckModified())
+      {
+        Item item = Context.ContentDatabase.GetItem(args.Parameters["Master"]);
+        if (item == null)
+        {
+          object[] parameters = new object[] { args.Parameters["Master"] };
+          SheerResponse.Alert(Translate.Text("Branch \"{0}\" not found.", parameters), Array.Empty<string>());
+        }
+        else if (item.TemplateID == TemplateIDs.CommandMaster)
+        {
+          string str = item["Command"];
+          if (!string.IsNullOrEmpty(str))
+          {
+            Context.ClientPage.SendMessage(this, str);
+          }
+        }
+        else if (args.IsPostBack)
+        {
+          if (args.HasResult)
+          {
+            string[] values = new string[] { args.Parameters["ItemID"] };
+            string str2 = StringUtil.GetString(values);
+            string[] textArray2 = new string[] { args.Parameters["Language"] };
+            string name = StringUtil.GetString(textArray2);
+            Item parent = Context.ContentDatabase.Items[str2, Language.Parse(name)];
+            if (parent == null)
+            {
+              SheerResponse.Alert("Parent item not found.", Array.Empty<string>());
+            }
+            else if (!parent.Access.CanCreate())
+            {
+              Context.ClientPage.ClientResponse.Alert("You do not have permission to create items here.");
+            }
+            else
+            {
+              Item item3 = null;
+              try
+              {
+                if (item.TemplateID == TemplateIDs.BranchTemplate)
+                {
+                  BranchItem branch = item;
+                  item3 = Context.Workflow.AddItem(args.Result, branch, parent);
+                  string[] parameters = new string[] { AuditFormatter.FormatItem((Item)branch) };
+                  Log.Audit(this, "Add from branch: {0}", parameters);
+                }
+                else
+                {
+                  TemplateItem template = item;
+                  item3 = Context.Workflow.AddItem(args.Result, template, parent);
+                  string[] parameters = new string[] { AuditFormatter.FormatItem((Item)template) };
+                  Log.Audit(this, "Add from template: {0}", parameters);
+                }
+              }
+              catch (WorkflowException exception)
+              {
+                Log.Error("Workflow error: could not add item from master", exception, this);
+                SheerResponse.Alert(exception.Message, Array.Empty<string>());
+              }
+              if ((item3 != null) && (this.ItemCreated != null))
+              {
+                this.ItemCreated(this, new ItemCreatedEventArgs(item3));
+              }
+            }
+          }
+        }
+        else
+        {
+          SheerResponse.Input("Enter a name for the new item:", item.DisplayName, Settings.ItemNameValidation, "'$Input' is not a valid name.", Settings.MaxItemNameLength);
+          args.WaitForPostBack();
+        }
+      }
+    }
+
+    public override void Execute(CommandContext context)
+    {
+      if ((context.Items.Length == 1) && context.Items[0].Access.CanCreate())
+      {
+        Item item = context.Items[0];
+        NameValueCollection parameters = new NameValueCollection
+        {
+          ["Master"] = context.Parameters["master"],
+          ["ItemID"] = item.ID.ToString(),
+          ["Language"] = item.Language.ToString(),
+          ["Version"] = item.Version.ToString()
+        };
+        Context.ClientPage.Start(this, "Add", parameters);
+      }
+    }
+
+    public override CommandState QueryState(CommandContext context)
+    {
+      Error.AssertObject(context, "context");
+      if (context.Items.Length != 1)
+      {
+        return CommandState.Hidden;
+      }
+      if (!context.Items[0].Access.CanCreate())
+      {
+        return CommandState.Disabled;
+      }
+      return base.QueryState(context);
+    }
+  }
+
+}
